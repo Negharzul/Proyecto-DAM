0e85b767a9defee43bf902552e82885712012d72
Antonio <antoniongh@gmail.com>
2025-03-22 21:49:41 +0100

    Setting up the project

---
+++ enlaceFP/src/main/java/com/enlaceFP/enlaceFP/EnlaceFpApplication.java
@@ -0,0 +1,13 @@
+package com.enlaceFP.enlaceFP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnlaceFpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnlaceFpApplication.class, args);
+	}
+
+}
